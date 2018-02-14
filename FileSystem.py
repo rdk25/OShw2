5fd142db4fcb75e858be01d4ee04f6dfdb677c31
emmettra <31417894+emmettra@users.noreply.github.com>
2018-02-13 22:07:27 -0800

    Update FileSystem.py

--- FileSystem.py
+++ FileSystem.py
@@ -3,9 +3,17 @@ from numpy import *
 
 class FileSystem:
 
+
+
 @staticmethod
 def createFileSystem(filename,block_count,magicNumber,blocksize=1024,inode_count):
 	blockdevice = BlockDevice("blockdevice",block_count,True)
-	buff = [magicnum, 1024, block_count, inode_count, 1, 2, 3]
-	packed_buf = numpy.packbits(buff)
+	buff = [magicnum] + [1024] + [block_count] + [inode_count] + [1, 2, 3, 0]
+	packed_buff = numpy.packbits(buff)
+	print(packed_buff)
 	blockdevice.write_block(0,packed_buff)
+	buff2 = [1,1]+[0,0,0,0,0,0,|]+[0,0,0,0,0,0,0,0,|]*(block_count/8-8)
+	packed_buff2 = numpy.packbits(buff2)
+	print(packed_buff2)
+	blockdevice.write_block(1,packed_buff2)
+	
