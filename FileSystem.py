1f6fb7619d63920cdd505ff857424bb7b7b18c22
rdk25 <rkaner25@gmail.com>
2018-02-17 17:28:42 -0800

    Update FileSystem.py

--- FileSystem.py
+++ FileSystem.py
@@ -12,7 +12,8 @@ def createFileSystem(filename,block_count,magicNumber,blocksize=1024,inode_count
 	packed_buff = numpy.packbits(buff)
 	print(packed_buff)
 	blockdevice.write_block(0,packed_buff)
-	buff2 = [1,1]+[0,0,0,0,0,0,|]+[0,0,0,0,0,0,0,0,|]*(block_count/8-8)
+	buff2 = ([1,1]+[0,0,0,0,0,0,|]+[0,0,0,0,0,0,0,0,|]*(block_count/8-8))[:-1] #added silly workaround to not print | at end of map
+
 	packed_buff2 = numpy.packbits(buff2)
 	print(packed_buff2)
 	blockdevice.write_block(1,packed_buff2)
@@ -26,12 +27,34 @@ def createFileSystem(filename,block_count,magicNumber,blocksize=1024,inode_count
 class BlockMap:
 
 	def __init__(self):
+		self.map = ([1,1]+[0,0,0,0,0,0,|]+[0,0,0,0,0,0,0,0,|]*(block_count/8-8))[:-1] 
+
 
 	def alloc_block(self):
-
+		i = 0
+		for thing in self.map:
+			if thing == 0:
+				thing == 1
+				return i
+			elif thing == 1:
+					i += 1
+			else:
+				pass
 
 	def free_block(self,num):
-
+		if num == 0 or 1 or 2:
+			return ("You can't fucking do that!")
+		else:
+			i = 0
+			for thing in self.map:
+				if thing == | :
+					pass
+				else:
+					if i == num:
+						thing == 0
+						return
+					else:
+						i += 1
 
 
 class iNode:
@@ -40,5 +63,6 @@ class iNode:
 
 
 	def free_inode(num):
+
 
 	
